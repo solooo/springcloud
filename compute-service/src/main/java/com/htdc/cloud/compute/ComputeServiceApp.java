50781aa10e8541fd7ef44118079b043041f44809
solooo <pjian108@gmail.com>
2017-10-19 17:07:30 +0800

    提供者注解修改

--- compute-service/src/main/java/com/htdc/cloud/compute/ComputeServiceApp.java
+++ compute-service/src/main/java/com/htdc/cloud/compute/ComputeServiceApp.java
@@ -2,6 +2,7 @@ package com.htdc.cloud.compute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Date:2017/8/14
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ComputeServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(ComputeServiceApp.class, args);
